cc544e433f8a8b544fcdc50b2e8f81be68049bc3
Alex Sinyavtsev <sinyavtsev.alek@yandex.ru>
2023-05-24 23:33:24 +0300

    add even-game

---
+++ src/modules/even.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace src\modules\even;
+
+use function src\modules\startGame;
+use const src\modules\engine\ATTEMPT_COUNT;
+
+const RULES = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+function isEven(int $n): string {
+	if ($n % 2 === 0) {
+		return 'yes';
+	} else {
+		return 'no';
+	}
+}
+
+function isCorrect(string $answer, int $number): bool {
+	if ($answer === isEven($number)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function run() {
+	$gameData = [];
+	$userAttempts = 0;
+	while ($userAttempts < ATTEMPT_COUNT) {
+		$number = rand(1, 100);
+		$correctAnswer = isEven($number);
+		$question = $number;
+		$gameData[] = [$question, $correctAnswer];
+		$userAttempts++;
+	}
+	startGame(RULES, $gameData);
+}
