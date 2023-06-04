2189a9e1456b87f592166a2ee34f59b09da81d7f
Alex Sinyavtsev <sinyavtsev.alek@yandex.ru>
2023-06-04 14:04:25 +0300

    correction progression.php

--- src/modules/progression.php
+++ src/modules/progression.php
@@ -3,33 +3,38 @@
 namespace src\modules\progression;
 
 use function src\modules\Engine\startGame;
+
 use const src\modules\Engine\ATTEMPT_COUNT;
 
 const RULES = 'What number is missing in the progression?';
 
-function generateProgression($startItem, $step, $itemsCount): array {
+function generateProgression($startItem, $step, $itemsCount): array 
+{
     $prog = [];
     $index = 0;
     while ($index < $itemsCount) {
         $prog[] = $startItem;
 	$startItem += $step;
-        $index++;	
+        $index++;
     }
     return $prog;
 }
 
-function makeKeyNumber($array, $keyIndex): int {
+function makeKeyNumber($array, $keyIndex): int
+{
     $keyNumber = $array[$keyIndex];
     return $keyNumber;
 }
 
-function prepareProgression($array, $keyIndex): array {
+function prepareProgression($array, $keyIndex): array
+{
     $temp = $array;
     $temp[$keyIndex] = '..';
     return $temp;
 }
 
-function isCorrect(string $answer, $array, $keyIndex): bool {
+function isCorrect(string $answer, $array, $keyIndex): bool
+{
     if ($answer ===  makeKeyNumber($array, $keyIndex)) {
         return true;
     } else {
@@ -37,20 +42,21 @@ function isCorrect(string $answer, $array, $keyIndex): bool {
     }
 }
 
-function run() {
+function run()
+{
     $gameData = [];
     $userAttempts = 0;
         while ($userAttempts < ATTEMPT_COUNT) {
-	    $startItem = rand(1, 100);
+            $startItem = rand(1, 100);
             $itemsCount = 10;
-	    $step = rand(1, 10);
-	    $keyIndex = rand(0, 9);
-	    $progression = generateProgression($startItem, $step, $itemsCount); 
-	    $correctAnswer = strval(makeKeyNumber($progression, $keyIndex));
-
+            $step = rand(1, 10);
+            $keyIndex = rand(0, 9);
+            $progression = generateProgression($startItem, $step, $itemsCount); 
+            $correctAnswer = strval(makeKeyNumber($progression, $keyIndex));
             $question = implode(' ', prepareProgression($progression, $keyIndex));
-	    $gameData[] = [$question, $correctAnswer];
-	    $userAttempts++;
+            $gameData[] = [$question, $correctAnswer];
+            $userAttempts++;
         }
         startGame(RULES, $gameData);
 }
+
