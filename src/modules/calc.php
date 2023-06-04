313340790d4714a95c36eb3e7771a032e0e85555
Alex Sinyavtsev <sinyavtsev.alek@yandex.ru>
2023-06-04 15:39:19 +0300

    correction calc.php, even.php, prime.php

--- src/modules/calc.php
+++ src/modules/calc.php
@@ -3,49 +3,53 @@
 namespace src\modules\calc;
 
 use function src\modules\Engine\startGame;
+
 use const src\modules\Engine\ATTEMPT_COUNT;
 
 const RULES = 'What is the result of the expression?';
 
-function calculate($number1, $number2, $sign) {
+function calculate($number1, $number2, $sign)
+{
     switch($sign) {
-	case '+':
-	    return $number1 + $number2;
-	    break;
-	case '-':
-	    return $number1 - $number2;
-	    break;
-	case '*':
-	    return $number1 * $number2;
-	    break;
+        case '+':
+            return $number1 + $number2;
+            break;
+        case '-':
+            return $number1 - $number2;
+            break;
+        case '*':
+            return $number1 * $number2;
+            break;
     }
 }
 
-function isCorrect(string $answer, int $number1, $number2, $sign): bool {
+function isCorrect(string $answer, int $number1, $number2, $sign): bool
+{
     if ($answer === calculate($number1, $number2, $sign)) {
         return true;
     } else {
-	return false;								            }
+        return false;								            }
     }
 
-function choiceSign() {
+function choiceSign()
+{
     $signs = ['+', '-', '*'];
     $sign = $signs[rand(0, 2)];
     return $sign;
 }
 
-function run() {
+function run()
+{
     $gameData = [];
     $userAttempts = 0;
     while ($userAttempts < ATTEMPT_COUNT) {
-	$number1 = rand(1, 100);
+        $number1 = rand(1, 100);
         $number2 = rand(1, 100);
         $sign = choiceSign();
-	$correctAnswer = strval(calculate($number1, $number2, $sign));
-	$question = "$number1 $sign $number2";
-	$gameData[] = [$question, $correctAnswer];
-	$userAttempts++;
+        $correctAnswer = strval(calculate($number1, $number2, $sign));
+        $question = "$number1 $sign $number2";
+        $gameData[] = [$question, $correctAnswer];
+        $userAttempts++;
     }
     startGame(RULES, $gameData);
 }
-
--- src/modules/prime.php
+++ src/modules/prime.php
@@ -3,42 +3,45 @@
 namespace src\modules\prime;
 
 use function src\modules\Engine\startGame;
+
 use const src\modules\Engine\ATTEMPT_COUNT;
 
 const RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-function isPrime($number): string {
+function isPrime($number): string
+{
     $primeNumbers = [2, 3, 5, 7, 11 ,13, 17, 19, 23, 29, 
-	31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73,
-	79, 83, 89, 97];
+        31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73,
+        79, 83, 89, 97];
     $resalt = 'no';
     for ($i = 0; $i < count($primeNumbers); $i++) {
-	    if ($number == $primeNumbers[$i]) {
-	        $resalt = 'yes';
-		return $resalt;
-		exit();
-	    }
+        if ($number == $primeNumbers[$i]) {
+           $resalt = 'yes';
+           return $resalt;
+           exit();
+        }
     }
     return $resalt;
 }
 
-function isCorrect(string $answer, int $number): bool {
+function isCorrect(string $answer, int $number): bool
+{
     if ($answer === isPrime($number)) {
         return true;
     } else {
         return false;                                                                       }
 }
 
-function run() {
+function run()
+{
     $gameData = [];
     $userAttempts = 0;
     while ($userAttempts < ATTEMPT_COUNT) {
         $number = rand(2, 100);
         $correctAnswer = isPrime($number);
-	$question = "$number";
+        $question = "$number";
         $gameData[] = [$question, $correctAnswer];
         $userAttempts++;
     }
     startGame(RULES, $gameData);
 }
-
